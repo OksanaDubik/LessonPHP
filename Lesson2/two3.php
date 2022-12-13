ecba2d1aca6d0c3fca3b51923cda75e5d1976ae3
Oksana Dubik <oksana170773@gmail.com>
2022-12-14 00:26:28 +0600

    массивы, count,array_rand, in_array,implode

--- Lesson2/two3.php
+++ Lesson2/two3.php
@@ -1,22 +1,23 @@
 <?php
-$numeric=(int)readline(" Загадай число ");
-$text = "Номер пальца = ";
-$number = $numeric % 8;
 
-switch ($number){
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5: echo $text .  $number;
-        break;
-    case 6: $number = $numeric - 2;
-        echo $text . $number;
-        break;
-    case 7: $number = ($numeric - 4) % 8;
-        echo $text . $number;
-        break;
-    case 0: $number = ($numeric - 6) % 8;
-        echo $text . $number;
-        break;
-}
+//$numeric=(int)readline(" Загадай число ");
+//$text = "Номер пальца = ";
+//$number = $numeric % 8;
+//
+//switch ($number){
+//    case 1:
+//    case 2:
+//    case 3:
+//    case 4:
+//    case 5: echo $text .  $number;
+//        break;
+//    case 6: $number = $numeric - 2;
+//        echo $text . $number;
+//        break;
+//    case 7: $number = ($numeric - 4) % 8;
+//        echo $text . $number;
+//        break;
+//    case 0: $number = ($numeric - 6) % 8;
+//        echo $text . $number;
+//        break;
+//}
---
+++ Lesson3/third.php
@@ -0,0 +1,8 @@
+<?php
+$arrOne = range(1 , 10);
+$arrTwo = range(1 , 10);
+$arrThird =[];
+for ($i = 0; $i < 10; $i++){
+    $arrThird[] += $arrOne[$i]*$arrTwo[$i];
+}
+print_r ($arrThird);
